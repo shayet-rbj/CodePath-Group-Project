c0cba9e7763eef540f7176e622660acdd148014c
shayet-rbj <xiayuli@usc.com>
2024-04-13 11:13:58 -0700

    create a project

---
+++ Chatter Box/Chatter_BoxApp.swift
@@ -0,0 +1,17 @@
+//
+//  Chatter_BoxApp.swift
+//  Chatter Box
+//
+//  Created by Shayet on 4/13/24.
+//
+
+import SwiftUI
+
+@main
+struct Chatter_BoxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
